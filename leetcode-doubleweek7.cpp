d5f9017f084b35abb3b7d188ccf9745011ca264a
Endeavour <736874054@qq.com>
2019-08-24 22:51:41 +0800

    双周赛以卵击石

---
+++ leetcode-doubleweek7.cpp
@@ -0,0 +1,12 @@
+clss Solution{
+	public:
+		int calculateTime(string keybord, string word){
+			for(int i = 0; i < keyboard.length; i++){
+				for(int j = 0; j < word.length; j++){
+					if(keyboard[j] == word[i]{
+						sum = sum + j;//其实还有个取位置步骤的但是我不会
+							}
+				}
+			}
+		}
+}
